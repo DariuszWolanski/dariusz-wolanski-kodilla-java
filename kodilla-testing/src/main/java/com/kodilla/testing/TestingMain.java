d8aa8ed84d9629223a9d139a1ae345612717c19a
Dariusz Wolanski <dariusz.wolanski@poczta.onet.pl>
2021-08-18 14:50:21 +0200

    6.2 Added kodilla-testing modul

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calc = new Calculator();
+        int result1 = calc.add(5, 65);
+        int result2 = calc.substract(5, 65);
+
+        if (result1 == 5 + 65){
+            System.out.println("Test additions method is OK");
+        } else {
+            System.out.println("Error - test additions method is failed!");
+        }
+
+        if (result2 == 5 - 65){
+            System.out.println("Test substract method is OK");
+        } else {
+            System.out.println("Error - test substract method is failed!");
+        }
+
+
+
+
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+
+
+    public int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public int substract(int a, int b){
+        int sub = a - b;
+        return sub;
+    }
+}
