f24e24d062a377f56a306a0bebe5c9c184d4143c
Dariusz Wolanski <dariusz.wolanski@poczta.onet.pl>
2021-09-15 11:42:55 +0200

    Exersise from modul 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage(){
+        //Given
+
+        int[] numbersTest = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200 };
+
+        //When
+
+        double averageTest = ArrayOperations.getAverage(numbersTest);
+
+        //Then
+
+        assertEquals(105, averageTest);
+
+    }
+}
