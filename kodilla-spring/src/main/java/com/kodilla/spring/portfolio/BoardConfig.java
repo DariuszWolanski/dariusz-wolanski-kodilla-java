acd36d6d6d37ca037a6d6fea5a4fc8bb84548d27
Dariusz Wolanski <dariusz.wolanski@poczta.onet.pl>
2021-11-05 18:12:36 +0100

    Module 14.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,27 +1,45 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
-    @Bean
-    public TaskList toDoList() {
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
+
+    @Bean(name = "toDoList")
+    @Scope("prototype")
+    public TaskList getToDoList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    @Scope("prototype")
+    public TaskList getInProgressList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "doneList")
+    @Scope("prototype")
+    public TaskList getDoneList(){
         return new TaskList();
     }
 
     @Bean
-    public TaskList inProgressList() {
-        return new TaskList();
-    }
-
-    @Bean
-    public TaskList doneList() {
-        return new TaskList();
-    }
-
-    @Bean
-    public Board board() {
-        return new Board(toDoList(), inProgressList(), doneList());
+    public Board getBoard(){
+        return new Board(toDoList, inProgressList, doneList);
     }
 }
